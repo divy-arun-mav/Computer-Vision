8a8c58a215f63605a0e914599c83a23e5f550988
Divy Mav <divymav5@gmail.com>
2024-03-31 15:39:50 +0530

    Pose-Estimation Completed

--- Pose-Estimation/PoseEstimationMin.py
+++ Pose-Estimation/PoseEstimationMin.py
@@ -6,7 +6,7 @@ mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 
-cap = cv2.VideoCapture('PoseVideos/Cycling Front View.avi')
+cap = cv2.VideoCapture('PoseVideos/human.mp4')
 
 pTime = 0
 
@@ -14,9 +14,14 @@ while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = pose.process(imgRGB)
-    print(results.pose_landmarks)
+    # print(results.pose_landmarks)
     if results.pose_landmarks:
         mpDraw.draw_landmarks(img,results.pose_landmarks,mpPose.POSE_CONNECTIONS)
+        for id,lm in enumerate(results.pose_landmarks.landmark):
+            h,w,c = img.shape
+            print(id,lm)
+            cx,cy = int(lm.x*w) ,int(lm.y*h)
+            cv2.circle(img,(cx,cy),5,(255,0,0),cv2.FILLED)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
